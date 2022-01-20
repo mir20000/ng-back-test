a0c00dd408880113ce6cb4dc4e8f6f363bf4c1c3
mir20000 <mir20000@users.noreply.github.com>
2022-01-20 21:21:15 +0530

    2 commit

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const dbconnect = require('./DB/dbconnect_promise');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const dbconnect = require("./DB/dbconnect_promise");
+const dotenv = require("dotenv");
 dotenv.config();
-const UserRoutes=require('./routes/userRoutes');
-const PostRoutes=require('./routes/postRoutes');
-const ContactRoutes=require('./routes/contactRoutes');
+const UserRoutes = require("./routes/userRoutes");
+const PostRoutes = require("./routes/postRoutes");
+const ContactRoutes = require("./routes/contactRoutes");
 
-const app=express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/user',UserRoutes);
-app.use('/post',PostRoutes);
-app.use('/contact',ContactRoutes);
+app.get("/", (req, res) => {
+  res.send("working...");
+});
+app.use("/user", UserRoutes);
+app.use("/post", PostRoutes);
+app.use("/contact", ContactRoutes);
 
-
-
-var PORT = process.env.PORT || 4500;
-app.listen(PORT, () => console.log(`EXPRESS Server Started at Port No: ${PORT}`));
+var PORT = process.env.PORT || 7000;
+app.listen(PORT, () =>
+  console.log(`EXPRESS Server Started at Port No: ${PORT}`)
+);
